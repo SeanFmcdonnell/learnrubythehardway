0d8689f60a8f2250fdc235d84f2b7a16b41b384c
Sean McDonnell <sean@freshpow.io>
2018-05-21 09:52:43 -0400

    more practice

--- ex9.rb
+++ ex9.rb
@@ -1,4 +1,18 @@
 # Here's some new strange stuff, remember type it exactly.
-
+#variable of a string
 days = "Mon Tue Wed Thu Fri Sat Sun"
-months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul"
+#variable of a string that starts a new line with each line
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+#printing out a string with the days variable
+puts "Here are the days: #{days}"
+#prints out the string with months variable
+puts "Here are the months: #{months}"
+
+#puts out a string in a quote block
+puts %q{
+  There's something going on here.
+  With this weird quote
+  We'll be able to type as much as we like.
+  Even 4 lines if we want, or 5, or 6.
+}
