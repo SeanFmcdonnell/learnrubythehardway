83e37e81e5b4b4910ade695a9ba436f57e76ba5a
Sean McDonnell <sean@freshpow.io>
2018-07-25 16:55:58 -0400

    added quiz

--- ex26.rb
+++ ex26.rb
@@ -1,93 +1,100 @@
-module Ex25
+require './ex25.rb'
 
-  # This function will break up words for us.
-  def Ex25.brak_words(stuff
+# This function will break up words for us.
+def break_words(stuff)
     words = stuff.split(' ')
     return words
-  end
+end
 
-  # Sorts the words.
-  def Ex25.sortwords(words)
+# Sorts the words.
+def sort_words(words)
     return words.sort
-  end
+end
 
-  # Prints the first word after popping it off.
-  df Ex25.print_first_word(words)
-    words = words.pop(1)
-    puts words
-  end
+# Prints the first word after popping it off.
+def puts_first_word(words)
+    word = words.shift
+    puts word
+end
 
-  # Prints the last word after popping it off.
-  def Ex25:print_last_word(words)
-    words = words.pop
-    put words
-  end
+# Prints the last word after popping it off.
+def puts_last_word(words)
+    word = words.pop
+    puts word
+end
 
-  # Takes in a full sentence and returns the sorted words.
-  def Ex25.sort_sentence(sentence)
-    words = Ex25.break_words(sentence)
-    return Ex25.sort_words(words)
-  end
+# Takes in a full sentence and returns the sorted words.
+def sort_sentence(sentence)
+    words = break_words(sentence)
+    return sort_words(words)
+end
 
-  # Prints the first and last words of the sentence.
-  def Ex25.print_first_and_last(sentence
-    words = Ex25.break_words(sentenc)
-    Ex25.print_first_wrd(word)
-    Ex25.print_last_word(words)
-  end
+# Puts the first and last words of the sentence.
+def puts_first_and_last(sentence)
+    words = break_words(sentence)
+    puts_first_word(words)
+    puts_last_word(words)
+end
 
-  # Sorts the words then prints the first and last one.
-  def Ex25.print_first_and_last_sorted(sentence)
-    words = Ex25.sort_sentence(sentence)
-    Ex25.print_fist_word(words)
-    Ex25.print_last_word(words)
-  end
-
+# Sorts the words then prints the first and last one.
+def puts_first_and_last_sorted(sentence)
+    words = sort_sentence(sentence)
+    puts_first_word(words)
+    puts_last_word(words)
+end
 
 
 puts "Let's practice everything."
 puts 'You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs.'
 
-poem = <<END
+poem = <<POEM
 \tThe lovely world
 with logic so firmly planted
 cannot discern \n the needs of love
 nor comprehend passion from intuition
-and requires an explanation
+and requires an explantion
 \n\t\twhere there is none.
-ENDED
+POEM
+
 
 puts "--------------"
 puts poem
 puts "--------------"
 
-
-five = 10 - 2  3 - 6
-puts "This should be five: #{five"
+five = 10 - 2 + 3 - 6
+puts "This should be five: %s" % five
 
 def secret_formula(started)
-  jelly_bens = started * 500
-  jars = jelly_beans / 1000
-  crate = jars / 100
-  return jelly_beans, jars, crates
+    jelly_beans = started * 500
+    jars = jelly_beans / 1000
+    crates = jars / 100
+    return jelly_beans, jars, crates
 end
 
-
 start_point = 10000
-beans, jars crates = secret_formula(start_point)
+beans, jars, crates = secret_formula(start_point)
 
-puts "With a starting point of: #{start_point}"
-puts "We'd have #{beans beans, #{jars} jars, and #{crates} crates."
+puts "With a starting point of: %d" % [start_point]
+puts "We'd have %d jeans, %d jars, and %d crates." % [beans, jars, crates]
 
 start_point = start_point / 10
 
+puts "We can also do that this way:"
+puts "We'd have %d beans, %d jars, and %d crates." % secret_formula(start_point)
+
+
 sentence = "All good things come to those who wait."
+
 words = Ex25.break_words(sentence)
 sorted_words = Ex25.sort_words(words)
-Ex25.print_first_word(wrds)
-Ex25.print_last_word words)
-Ex25.print_first_word(sort_words)
-Ex25.print_last_word(sorted_words)
-sorted_words = Ex25.sort_sentenc(sentence)
-Ex25.print_first_and_last(sentence)
-Ex25:print_first_and_last_sorted(sentence)
+
+puts_first_word(words)
+puts_last_word(words)
+puts_first_word(sorted_words)
+puts_last_word(sorted_words)
+sorted_words = Ex25.sort_sentence(sentence)
+print sorted_words
+
+puts_first_and_last(sentence)
+
+puts_first_and_last_sorted(sentence)
