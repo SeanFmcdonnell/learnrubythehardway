56b77908f59c472d6c15839d824a5d866b5c2d14
Sean McDonnell <sean@freshpow.io>
2018-07-30 16:34:33 -0400

    add ex33.rb

---
+++ ex33.rb
@@ -0,0 +1,21 @@
+def while_loop(number)
+  i = 0
+numbers = []
+
+while i < 6
+  puts "At the top i is #{i}"
+  numbers.push(i)
+
+  i += 1
+  puts "Numbers now: ", numbers
+  puts "At the bottom is is #{i}"
+end
+
+puts "The numbers: "
+
+#remember you can write this 2 other ways?
+numbers.each {|num| puts num }
+end
+
+while_loop(2)
+while_loop(6)
